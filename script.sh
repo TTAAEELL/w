5eafcd921546583e331b2077cdf49dc817f4abd5
TAEL <ttaaeell@gmail.com>
2024-06-01 02:49:54 +0330

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,455 @@
+#!/bin/bash
+
+#colors
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+white='\033[0;37m'
+rest='\033[0m'
+
+case "$(uname -m)" in
+	x86_64 | x64 | amd64 )
+	    cpu=amd64
+	;;
+	i386 | i686 )
+        cpu=386
+	;;
+	armv8 | armv8l | arm64 | aarch64 )
+        cpu=arm64
+	;;
+	armv7l )
+        cpu=arm
+	;;
+	* )
+	echo "The current architecture is $(uname -m), not supported"
+	exit
+	;;
+esac
+
+cfwarpIP() {
+    if [[ ! -f "$PREFIX/bin/warpendpoint" ]]; then
+        echo "Downloading warpendpoint program"
+        if [[ -n $cpu ]]; then
+            curl -L -o warpendpoint -# --retry 2 https://raw.githubusercontent.com/Ptechgithub/warp/main/endip/$cpu
+            cp warpendpoint $PREFIX/bin
+            chmod +x $PREFIX/bin/warpendpoint
+        fi
+    fi
+}
+
+endipv4(){
+	n=0
+	iplist=100
+	while true
+	do
+		temp[$n]=$(echo 162.159.192.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo 162.159.193.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo 162.159.195.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo 188.114.96.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo 188.114.97.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo 188.114.98.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo 188.114.99.$(($RANDOM%256)))
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+	done
+	while true
+	do
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 162.159.192.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 162.159.193.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 162.159.195.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 188.114.96.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 188.114.97.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 188.114.98.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo 188.114.99.$(($RANDOM%256)))
+			n=$[$n+1]
+		fi
+	done
+}
+
+endipv6(){
+	n=0
+	iplist=100
+	while true
+	do
+		temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2)))])
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+		temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2)))])
+		n=$[$n+1]
+		if [ $n -ge $iplist ]
+		then
+			break
+		fi
+	done
+	while true
+	do
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2)))])
+			n=$[$n+1]
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]
+		then
+			break
+		else
+			temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2))):$(printf '%x\n' $(($RANDOM*2+$RANDOM%2)))])
+			n=$[$n+1]
+		fi
+	done
+}
+
+freeCloudflareAccount(){
+	output=$(curl -sL "https://api.zeroteam.top/warp?format=sing-box" | grep -Eo --color=never '"2606:4700:[0-9a-f:]+/128"|"private_key":"[0-9a-zA-Z\/+]+="|"reserved":\[[0-9]+(,[0-9]+){2}\]')
+	publicKey=$(echo "$output" | grep -oP '("2606:4700:[0-9a-f:]+/128")' | tr -d '"')
+	privateKey=$(echo "$output" | grep -oP '("private_key":"[0-9a-zA-Z\/+]+=")' | cut -d':' -f2 | tr -d '"')
+	reserved=$(echo "$output" | grep -oP '(\[[0-9]+(,[0-9]+){2}\])' | tr -d '"' | sed 's/"reserved"://')
+}
+freeCloudflareAccount2(){
+	output=$(curl -sL "https://api.zeroteam.top/warp?format=sing-box" | grep -Eo --color=never '"2606:4700:[0-9a-f:]+/128"|"private_key":"[0-9a-zA-Z\/+]+="|"reserved":\[[0-9]+(,[0-9]+){2}\]')
+	publicKey2=$(echo "$output" | grep -oP '("2606:4700:[0-9a-f:]+/128")' | tr -d '"')
+	privateKey2=$(echo "$output" | grep -oP '("private_key":"[0-9a-zA-Z\/+]+=")' | cut -d':' -f2 | tr -d '"')
+	reserved2=$(echo "$output" | grep -oP '(\[[0-9]+(,[0-9]+){2}\])' | tr -d '"' | sed 's/"reserved"://')
+}
+
+endipresult() {
+    echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
+    ulimit -n 102400
+    chmod +x warpendpoint >/dev/null 2>&1
+    if command -v warpendpoint &>/dev/null; then
+        warpendpoint
+   else
+        ./warpendpoint
+    fi
+    
+    clear
+    Endip_v4=$(cat result.csv | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+" | head -n 1)
+	Endip_v4_ip="${Endip_v4%:*}"
+	Endip_v4_port="${Endip_v4##*:}"
+    
+	freeCloudflareAccount
+	freeCloudflareAccount2
+
+	template='
+ {
+   "log":{
+      "level":"warn",
+      "output":"box.log",
+      "timestamp":true
+   },
+   "dns":{
+      "servers":[
+         {
+            "tag":"dns-remote",
+            "address":"https://dnslow.me/dns-query",
+            "address_resolver":"dns-direct"
+         },
+         {
+            "tag":"dns-trick-direct",
+            "address":"https://sky.rethinkdns.com/",
+            "detour":"direct-fragment"
+         },
+         {
+            "tag":"dns-direct",
+            "address":"https://dnslow.me/dns-query",
+            "address_resolver":"dns-local",
+            "detour":"direct"
+         },
+         {
+            "tag":"dns-local",
+            "address":"local",
+            "detour":"direct"
+         },
+         {
+            "tag":"dns-block",
+            "address":"rcode://success"
+         }
+      ],
+      "rules":[
+         {
+            "domain":"dnslow.me",
+            "server":"dns-direct"
+         },
+         {
+            "domain_suffix":".ir",
+            "geosite":"ir",
+            "server":"dns-direct"
+         },
+         {
+            "domain":"cp.cloudflare.com",
+            "server":"dns-remote",
+            "rewrite_ttl":3000
+         }
+      ],
+      "final":"dns-remote",
+      "static_ips":{
+         "sky.rethinkdns.com":[
+            "172.67.204.84",
+            "104.21.69.44",
+            "2606:4700:3032::6815:452c",
+            "2606:4700:3031::ac43:cc54",
+            "104.18.203.232",
+            "104.18.202.232",
+            "172.64.172.4",
+            "172.64.173.4",
+            "2606:4700:e4::ac40:ad04",
+            "2606:4700:e4::ac40:ac04"
+         ]
+      },
+      "independent_cache":true
+   },
+   "inbounds":[
+      {
+         "type":"tun",
+         "tag":"tun-in",
+         "mtu":9000,
+         "inet4_address":"172.19.0.1/28",
+         "inet6_address":"fdfe:dcba:9876::1/126",
+         "auto_route":true,
+         "strict_route":true,
+         "endpoint_independent_nat":true,
+         "sniff":true,
+         "sniff_override_destination":true,
+         "domain_strategy":"prefer_ipv4"
+      },
+      {
+         "type":"mixed",
+         "tag":"mixed-in",
+         "listen":"127.0.0.1",
+         "listen_port":2334,
+         "sniff":true,
+         "sniff_override_destination":true,
+         "domain_strategy":"prefer_ipv4"
+      },
+      {
+         "type":"direct",
+         "tag":"dns-in",
+         "listen":"127.0.0.1",
+         "listen_port":6450,
+         "override_address":"1.1.1.1",
+         "override_port":53
+      }
+   ],
+   "outbounds":[
+      {
+         "type":"selector",
+         "tag":"select",
+         "outbounds":[
+            "auto",
+            "🇮🇷 WARP - Iran",
+            "🇩🇪 WARP - Germany"
+         ],
+         "default":"auto"
+      },
+      {
+         "type":"urltest",
+         "tag":"auto",
+         "outbounds":[
+            "🇮🇷 WARP - Iran",
+            "🇩🇪 WARP - Germany"
+         ],
+         "url":"http://cp.cloudflare.com/",
+         "interval":"10m0s"
+      },
+      {
+         "type":"wireguard",
+         "tag":"🇮🇷 WARP - Iran",
+         "local_address":[
+            "172.16.0.2/32",
+            "2606:4700:110:8c91:4063:21d0:7dd5:f218/128",
+            "'$publicKey'"
+         ],
+         "private_key":"'$privateKey'",
+         "server":"'$Endip_v4_ip'",
+         "server_port":"$Endip_v4_port",
+         "peer_public_key":"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+         "reserved":"$reserved",
+         "mtu":1280,
+         "fake_packets":"5-10",
+         "fake_packets_size":"40-100",
+         "fake_packets_delay":"20-250"
+      },
+      {
+         "type":"wireguard",
+         "tag":"🇩🇪 WARP - Germany",
+         "detour":"🇮🇷 WARP - Iran",
+         "local_address":[
+            "172.16.0.2/32",
+            "2606:4700:110:8c15:3f90:ad2d:8810:77f3/128",
+            "'$publicKey2'"
+         ],
+         "private_key":"'$privateKey2'",
+         "server":"'$Endip_v4_ip'",
+         "server_port":"$Endip_v4_port",
+         "peer_public_key":"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+         "reserved":"$reserved2",
+         "mtu":1280,
+         "fake_packets":"5-10",
+         "fake_packets_size":"40-100",
+         "fake_packets_delay":"20-250"
+      },
+      {
+         "type":"dns",
+         "tag":"dns-out"
+      },
+      {
+         "type":"direct",
+         "tag":"direct"
+      },
+      {
+         "type":"direct",
+         "tag":"direct-fragment",
+         "tls_fragment":{
+            "enabled":true,
+            "size":"5-10",
+            "sleep":"1-5"
+         }
+      },
+      {
+         "type":"direct",
+         "tag":"bypass"
+      },
+      {
+         "type":"block",
+         "tag":"block"
+      }
+   ],
+   "route":{
+      "geoip":{
+         "path":"geo-assets/sagernet-sing-geoip-geoip.db"
+      },
+      "geosite":{
+         "path":"geo-assets/sagernet-sing-geosite-geosite.db"
+      },
+      "rules":[
+         {
+            "inbound":"dns-in",
+            "outbound":"dns-out"
+         },
+         {
+            "port":53,
+            "outbound":"dns-out"
+         },
+         {
+            "clash_mode":"Direct",
+            "outbound":"direct"
+         },
+         {
+            "clash_mode":"Global",
+            "outbound":"select"
+         },
+         {
+            "domain_suffix":".ir",
+            "geosite":"ir",
+            "geoip":"ir",
+            "outbound":"bypass"
+         }
+      ],
+      "final":"select",
+      "auto_detect_interface":true,
+      "override_android_vpn":true
+   },
+   "experimental":{
+      "cache_file":{
+         "enabled":true,
+         "path":"clash.db"
+      },
+      "clash_api":{
+         "external_controller":"127.0.0.1:6756"
+      }
+   }
+}'
+
+	echo "$template"
+
+    rm warpendpoint >/dev/null 2>&1
+    rm -rf ip.txt
+	rm -rf result.csv
+    exit
+}
+
+
+cfwarpIP
+endipv4
+endipresult
+Endip_v4
